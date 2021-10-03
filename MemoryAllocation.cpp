6651b1ab0a24d76699eaa784e48c5c2b46a06e5c
sisbhumi <87226560+sisbhumi@users.noreply.github.com>
2021-10-03 16:25:03 +0530

    FirstFit BestFit WorstFit

--- MemoryAllocation.cpp
+++ MemoryAllocation.cpp
@@ -8,6 +8,8 @@ class MemoryAllocation
 	int *blockSize , *processSize , *allocation , *flag ;
 	void input() ;
 	void First_Fit() ;
+	void Best_Fit() ;
+	void Worst_Fit() ;
 	void output() ;
 };
 
@@ -54,12 +56,52 @@ void MemoryAllocation :: First_Fit()
 		}
 }
 
+void MemoryAllocation :: Best_Fit()
+{
+	
+	for( int i=0 ; i<amtProcess ; i++)
+	{
+		int min = INT_MAX ;
+		for( int j=0 ; j<amtBlocks ; j++  )
+		{
+			if( min > blockSize[j] - processSize[i] && blockSize[j] >= processSize[i] && flag[j] == 0 )
+			{
+				min = blockSize[j] - processSize[i] ;
+				allocation[i] = j ;
+				flag[j] = 1;
+				 // ith process is allocated to jth memory block
+			}
+		}
+	}
+}
+
+void MemoryAllocation :: Worst_Fit()
+{
+	
+	for( int i=0 ; i<amtProcess ; i++)
+	{
+		int max = 0 , tempflag = -1;
+		for( int j=0 ; j<amtBlocks ; j++  )
+		{
+			if( max <= ( blockSize[j] - processSize[i] )  && ( blockSize[j] >= processSize[i] ) && ( flag[j] == 0) )
+			{
+				max = blockSize[j] - processSize[i] ;
+				allocation[i] = j ;
+				tempflag = j;
+				// ith process is allocated to jth memory block
+			}
+		}
+		if( tempflag != -1 )
+			flag[tempflag] = 1;
+	}
+}
+
 void MemoryAllocation :: output()
 {
 	cout<<"Process no.\tProcess size\tMemory Block\tMemory Size\n";
 	for( int i=0 ; i<amtProcess ; i++ )
 	{
-		cout<<i<<"\t"<<processSize[i]<<"\t";
+		cout<<i<<"\t\t"<<processSize[i]<<"\t\t";
 		if( flag[allocation[i]] == 1 )
 		{
 			cout<<allocation[i]<<"\t\t"<<blockSize[allocation[i]]<<"\n";
@@ -70,10 +112,27 @@ void MemoryAllocation :: output()
 
 int main()
 {
-	MemoryAllocation pc;
-	pc.input();
-	pc.First_Fit();
-	pc.output();
+	MemoryAllocation ff, bf , wf;
+	int choice;
+	cout<<"Enter '1' for First Fit.\n'2' for Best Fit.\n'3' for Worst Fit.\n";
+	cin>>choice;
 	
+	switch(choice)
+	{
+		case 1: ff.input();
+				ff.First_Fit();
+				ff.output();
+				//break;
+		case 2: bf.input();
+				bf.Best_Fit();
+				bf.output();
+				//break;
+		case 3: wf.input();
+				wf.Worst_Fit();
+				wf.output();
+				break;
+		default : cout<<"Wrong Choice.\n";
+			break;
+	}
 	
 }
